72a4520fd0c08653ba59b13b9ff88d9292a96a83
Hassan Raza <hassanraza74659@gmail.com>
2026-08-10 06:29:55 +0500

    test

--- resources/views/all_notifications.blade.php
+++ resources/views/all_notifications.blade.php
@@ -9,7 +9,7 @@
         <header class="header--dashboard">
 
             <div class="header__left">
-                <h3>Notifications ({{count($notifications)}})</h3>
+                <h3>Notificationssssss ({{count($notifications)}})</h3>
             </div>
 
         </header>
